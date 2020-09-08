c81d83127eb1b31a910b6efa5af9e77f0add825e
Thiago Hersan <thiago.hersan+github@gmail.com>
2020-09-08 18:00:30 -0300

    thank you page

---
+++ _pages/3-thankyou.md
@@ -0,0 +1,12 @@
+---
+layout: home
+title: The Happy App
+permalink: /thankyou/
+---
+<div class="page-3-content">
+  <div class="page-3-container">
+    <div id="my-thankyou-text" class="page-3-thankyou"></div>
+    <div class="page-3-logo"></div>
+  </div>
+</div>
+<script src="{{ site.baseurl }}/js/thankyou.js"></script>
---
+++ js/thankyou.js
@@ -0,0 +1,13 @@
+const txts = [
+  'thank you',
+  'obrigado'
+];
+
+function changeThankYou() {
+  const thankyouElement = document.getElementById('my-thankyou-text');
+  const nextTxt = txts[(Math.floor(Date.now() / 1000)) % txts.length];
+  thankyouElement.innerHTML = nextTxt;
+  setTimeout(changeThankYou, 1000);
+}
+
+document.addEventListener('DOMContentLoaded', changeThankYou);
