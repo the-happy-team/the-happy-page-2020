eb74e8384c6813707b1d1ecaad618ae8799047d0
Thiago Hersan <thiago.hersan+github@gmail.com>
2020-09-08 17:13:23 -0300

    update gohome script for 404

--- js/go-home.js
+++ js/go-home.js
@@ -1,9 +1,4 @@
 ---
 layout: none
 ---
-
-document.addEventListener('DOMContentLoaded', () => {
-  if(window.location.href.includes('404.html')) {
-    window.location.href = '{{ site.baseurl }}/';
-  }
-});
+window.location.href = '{{ site.baseurl }}/';
